8fb76a7c93515b3e09e475a3d1b12413e1c5f0b6
matz97 <barroamatheus98gta@gmail.com>
2026-03-17 09:37:15 -0300

    Delete ex06.py

--- ex06.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Quantidade de alunos: "))
-soma = 0
-
-for i in range(n):
-    nota = float(input("Digite a nota: "))
-    soma += nota
-
-media = soma / n
-print("Média das notas:", media)
